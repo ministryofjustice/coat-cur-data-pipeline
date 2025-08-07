cbd3d68c7d692947b237fbc57818e5a2805b4fa1
tam <tamsin.forbes@digital.justice.gov.uk>
2025-08-07 08:22:31 +0000

    scripts to read csv and write to partitioned parquet

---
+++ scripts/test-data.py
@@ -0,0 +1,31 @@
+import awswrangler as wr
+import pandas as pd
+
+# Delete old experiements
+# wr.s3.delete_objects('s3://alpha-coat-moj-cur-v2-hourly/coat_cur_test_data/') 
+
+# Create database: run once only
+# wr.catalog.create_database(
+#     name='cloud_optimisation_and_accountabilty'
+# )
+
+# Read data from csv file in bucket
+df = wr.s3.read_csv(path='s3://alpha-coat-moj-cur-v2-hourly/cur_row_900000_to_1000000.csv')
+print( df.head(10))
+
+# Write data to parquet with partitions and create table in database
+# Every time this writes it appends data in partitions, so can duplicate data 
+wr.s3.to_parquet(
+    df=df,
+    path='s3://alpha-coat-moj-cur-v2-hourly/coat_cur_test_data/test_data.parquet',
+    dataset=True,
+    partition_cols=['billing_period'],
+    database='cloud_optimisation_and_accountabilty',
+    table='coat_cur_test_data',
+    dtype={
+        'bill_billing_period_end_date': 'timestamp',
+        'bill_billing_period_start_date': 'timestamp',
+        'billing_period': 'string'
+    }
+)
+
