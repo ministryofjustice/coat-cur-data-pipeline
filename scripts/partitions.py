1bd84d8ff7477bf21168fb304ec3a34da4c8203f
mamonu <thorosm@msn.com>
2026-03-03 09:56:00 +0000

    modularise code and add tests

---
+++ scripts/partitions.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+
+def filter_billing_periods(billing_period_list, date_limit=datetime.now() - relativedelta(years=1)):
+    return [
+        bp for bp in billing_period_list
+        if bp.startswith("BILLING_PERIOD")
+        and datetime.strptime(bp.split('=')[1], "%Y-%m") >= date_limit
+    ]
+
+
+def create_partition_map(path_to_partitions, partition_list):
+    partitions_values = {}
+
+    for billing_period in partition_list:
+        key = path_to_partitions + billing_period + "/"
+        value = [billing_period.split("=")[1]]
+        partitions_values[key] = value
+
+    return partitions_values
